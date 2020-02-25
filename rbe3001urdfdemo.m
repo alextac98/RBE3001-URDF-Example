5e7c82db9444bf8e8854b3d894fc4b6475547dc1
Alex Tacescu <alextac98@gmail.com>
2020-02-25 16:27:15 -0500

    Add demo code

--- rbe3001urdfdemo.m
+++ rbe3001urdfdemo.m
@@ -1,22 +1,15 @@
+clear;
+clc;
+
 robot = makeRobot('rbe3001arm.urdf');
 robot_cfg = homeConfiguration(robot);
 
-robot_med = makeRobot('rbe3001arm_medres.urdf');
-robot_med_cfg = homeConfiguration(robot_med);
 
-tic;
-for i = 1:1:10
-   dispRobot(robot, moveRobot(robot_cfg, pi/i, -pi/i, -pi/i));
-   pause(0.01);
-end
-disp("Low Res Model Avg Cycle Time: "+ toc/10);
-
-tic;
-for i = 1:1:10
-   dispRobot(robot_med, moveRobot(robot_med_cfg, pi/i, -pi/i, -pi/i));
-   pause(0.01);
-end
-disp("Med Res Model Avg Cycle Time: "+ toc/10);
+    for i = 1:1:10
+       robot_cfg = moveRobot(robot_cfg, pi/i, -pi/i, -pi/i);
+       dispRobot(robot, robot_cfg);
+       pause(0.01);
+    end
 
 function robot=makeRobot(urdf_loc)
     robot = importrobot(urdf_loc);
@@ -31,6 +24,6 @@ end
 
 function dispRobot(robot, config)
     show(robot, config);
-    %axis([-0.25, 0.4, -0.2, 0.2, -0.05, 0.4]);
+    axis([-0.25, 0.4, -0.2, 0.2, -0.05, 0.4]);
     axis off
 end
